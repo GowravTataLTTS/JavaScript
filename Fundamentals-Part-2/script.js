4dd6408ac8704dc71884a36477a8066392e1ce6a
Gowrav Tata <54976985+GowravTata@users.noreply.github.com>
2023-09-23 14:13:27 +0000

    Added Functions

--- Fundamentals-Part-2/script.js
+++ Fundamentals-Part-2/script.js
@@ -19,14 +19,78 @@ const passTest = true;
 if (hasDriversLicense) console.log('I can Drive')
 console.log(hasDriversLicense)
 
-const private = 23;
+// const private = 23; // Uncomment this line to see 
 
 /*
-FUNCTIONS:
+FUNCTIONS: 
+functions are a piece of code, which can be used to write some logic and store it and
+reuse it for many times
 */ 
 
 function logger() {
     console.log('My name is Gowrav');
 }
 
-logger() // Calling/Running/Invoking the function 
+logger() // Calling/Running/Invoking the function, by referring the name with params
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice
+}
+
+const appleJuice = fruitProcessor(4,5) ;
+console.log(appleJuice);
+// Function is invoked and the value is stored in a variable and the same is printed
+
+// In one line
+console.log(fruitProcessor(2,3));
+
+
+/* 
+In the above defined function , no parameters, but it is invoked with 2 params, that params will not
+be considered while executing the function
+*/
+logger(2,3)
+
+
+/*
+FUNCTIONS : DECLARATIONS VS EXPRESSIONS
+The main difference between functions and declarations is declaration can be called from anywhere
+meaning , function declaration can be called before it is defined , this is know as HOISTING
+function expression can only be used only after it is declared
+*/
+// Define a function which defines the age based on the input
+// Function Declaration
+// console.log(calcAge(1996)); // Uncomment this line to see function declaration can be called before it is defined
+
+function calcAge(birthYear) {
+    return `Your age is ${2023 - birthYear} years.`
+}
+
+//const age1 = console.log(calcAge(1996));
+
+//console.log(calcAge2(1996)); // Uncomment this line to see if function expression can be called before definining
+
+// Function Expression
+const calcAge2  = function (birthYear) {
+    return `Your age is ${2023 - birthYear} years.`
+}
+const age1 = calcAge(1996);
+const age2 = calcAge2(1996);
+console.log(age1, age2);
+
+/*
+ARROW FUNCTIONS
+*/
+
+const calcAge3 = birthYear => 2023 - birthYear;
+const age3 = calcAge3(1996);
+console.log(age3);
+
+const yearUntilRetirement = (birthYear, firstName) => {
+    const age = 2023 - birthYear;
+    const retirement = 60 - age
+    return `${firstName} will retire in ${retirement} years`
+}
+
+console.log(yearUntilRetirement(1996, 'Gowrav'));
