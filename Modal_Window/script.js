58ebca522e025c292207f001230d28c7db6b614e
GowravTata <gowravsaitata@gmail.com>
2023-09-28 22:21:55 +0530

    Added Modal Window project

---
+++ Modal_Window/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+// If the multiple elements have the same class, then use the following
+const btnsOpnModal = document.querySelectorAll('.show-modal')
+
+console.log(btnsOpnModal);
